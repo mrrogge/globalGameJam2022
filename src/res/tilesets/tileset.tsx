2bfeb11af5477b58abebb21d74e6a5ce1acc6e54
mrrogge <5900418+mrrogge@users.noreply.github.com>
2022-01-29 01:57:45 -0600

    starting on some assets

---
+++ src/res/tilesets/tileset.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="tileset" tilewidth="16" tileheight="16" tilecount="400" columns="20">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <image source="../img/tileset.png" width="320" height="320"/>
+</tileset>
