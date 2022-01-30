5d7c473b980de2b7660524acd40469600ba4e47c
mrrogge <5900418+mrrogge@users.noreply.github.com>
2022-01-29 22:22:13 -0600

    bullet stuff

---
+++ src/com/BulletData.hx
@@ -0,0 +1,11 @@
+package com;
+
+class BulletData {
+    public var velX:Float;
+    public var velY:Float;
+
+    public function new(velX=0., velY=0.) {
+        this.velX = velX;
+        this.velY = velY;
+    }
+}
